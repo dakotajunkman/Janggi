69e5517f2df5d083cfc495aff8cbf9a6dd7c206a
Dakota Junkman <dakota.junkman@gmail.com>
2021-02-22 19:33:05 -0600

    horse block tested.

--- JanggiGame.py
+++ JanggiGame.py
@@ -573,6 +573,40 @@ class Horse(MasterPiece):
             return True
         return False
 
+    def is_blocked(self, next_loc: tuple, board) -> bool:
+        """
+        Determines whether a piece is blocked from moving to it's desired location. Soldier, guard 
+        and general will inherit this method. Does not take in to account if the space is a valid move 
+        for the piece.
+        param next_loc: desired move-to location of the piece
+        param board: the current game board
+        return: True if piece is blocked, else False
+        """
+        if board.get_piece(next_loc) is not None and board.get_piece(next_loc).get_color() == self._color:
+            return True
+
+        cur_loc = self._location
+
+        # check horizontal then diagonal
+        if abs(next_loc[0] - cur_loc[0]) == 1:
+            if cur_loc[1] < next_loc[1]:
+                cur_loc = (cur_loc[0], cur_loc[1] + 1)
+            else:
+                cur_loc = (cur_loc[0], cur_loc[1] - 1)
+        
+        # check vertical then diagonal
+        else:
+            if cur_loc[0] < next_loc[0]:
+                cur_loc = (cur_loc[0] + 1, cur_loc[1])
+            else:
+                cur_loc = (cur_loc[0] - 1, cur_loc[1])        
+
+        # do not need to check the destination since it was checked above
+        # only need to check the orthogonal move
+        if board.get_piece(cur_loc) is not None:
+            return True
+        return False   
+
 
 class Guard(MasterPiece):
     """
